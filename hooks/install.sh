d8d2d399a5418fb292214198a72094177c977d6c
Robert <zykon@pm.me>
2024-08-29 21:16:43 +0300

    updated docker

--- hooks/install.sh
+++ hooks/install.sh
@@ -59,3 +59,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Run at restart
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
